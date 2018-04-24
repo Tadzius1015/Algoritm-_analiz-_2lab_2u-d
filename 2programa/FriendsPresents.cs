881aea36be23fb442416a914eb07609b6bb53a5b
Tadzius1015 <wtadasw@gmail.com>
2018-04-24 13:01:01 +0300

    2 uzduotis

---
+++ 2programa/FriendsPresents.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_programa
+{
+    class FriendsPresents
+    {
+        private int[] PresentsPrice;
+        private List<int> FriendA { get; set; }
+        private List<int> FriendB { get; set; }
+        private int Substraction;
+        
+        public FriendsPresents(int[] presentsPrice, List<int> friendA, List<int> friendB, int substraction)
+        {
+            PresentsPrice = presentsPrice;
+            FriendA = friendA;
+            FriendB = friendB;
+            Substraction = substraction;
+        }
+        public FriendsPresents(int[] presentsPrice)
+        {
+            PresentsPrice = presentsPrice;
+        }
+        public FriendsPresents() { }
+        public int getPresentValue(int index)
+        {
+            return PresentsPrice[index];
+        }
+        public int getSubstraction()
+        {
+            return this.Substraction;
+        }
+        public string subSeq(int index)
+        {
+            return "1 Draugės dovanų vertės: " + string.Join(",", FriendA) + " || " + "2 Draugės dovanų vertės: " + string.Join(",", FriendB);
+        }
+        public string subSeqq(int index)
+        {
+            return "1 Draugės dovanų vertės: " + string.Join(",", FriendB) + " || " + "2 Draugės dovanų vertės: " + string.Join(",", FriendA);
+        }
+        public int getFriendAValue(int index)
+        {
+            return FriendA.ElementAt(index);
+        }
+        public int getFriendBValue(int index)
+        {
+            return FriendB.ElementAt(index);
+        }
+        public void setFriendAValue(int index, int value)
+        {
+            FriendA[index] = value;
+        }
+        public void setFriendBValue(int index, int value)
+        {
+            FriendB[index] = value;
+        }
+        public int getLength()
+        {
+            return FriendA.Count;
+        }
+        public void addPresentFriendA(int value)
+        {
+            FriendA.Add(value);
+        }
+        public void addPresentFriendB(int value)
+        {
+            FriendB.Add(value);
+        }
+        public void removePresentFriendB(int index)
+        {
+            FriendB.Remove(index);
+        }
+        public int ListASum()
+        {
+            return FriendA.Sum();
+        }
+        public int ListBSum()
+        {
+            return FriendB.Sum();
+        }
+        public FriendsPresents CloneFriendA()
+        {
+            return new FriendsPresents(this.PresentsPrice, CloneList(FriendA), CloneList(FriendB), this.Substraction);
+        }
+        public List<int> CloneList(List<int> a)
+        {
+            List<int> tmp = new List<int>();
+            for (int i = 0; i < a.Count; i++)
+            {
+                tmp.Add(a.ElementAt(i));
+            }
+            return tmp;
+        }
+        public FriendsPresents Give(int n, int m)
+        {
+            int value = D(n, m);
+            this.Substraction = this.Substraction + value;
+            return this;
+        }
+
+        private int D(int n, int m)
+        {
+            if (m == 0)
+            {
+                addPresentFriendA(this.PresentsPrice[n]);
+                return this.PresentsPrice[n];
+            }
+
+            if (m == 1)
+            {
+                addPresentFriendB(this.PresentsPrice[n]);
+                return -1 * this.PresentsPrice[n];
+            }
+
+            return 0;
+        }
+
+    }
+}
---
+++ 2programa/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _2_programa
+{
+    class Program
+    {
+        static List<FriendsPresents> var = new List<FriendsPresents>();
+        static int operationsCount = 0;
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            int[] presentsPrice = { 10, 20, 40, 11, 5, 6, 119, 54, 25, 65, 77, 88, 215, 456, 200, 69, 41, 56, 12, 4, 898, 444, 11 };
+            List<int> frA = new List<int>();
+            List<int> frB = new List<int>();
+            int k = 0;
+            FriendsPresents all = new FriendsPresents(presentsPrice, frA, frB, k);
+            timer.Start();
+            Split(all, presentsPrice.Length, -1);
+            timer.Stop();
+            TimeSpan time = timer.Elapsed;
+            timer.Reset();
+            List<string> seq = Sequences();
+            Console.WriteLine("Operacijų kiekis: " + operationsCount);
+            Console.WriteLine("Dinaminis programavimas užtruko: " + time);
+            Console.WriteLine("Atlikta");
+        }
+        public static void Split(FriendsPresents presents, int n, int m)
+        {
+            operationsCount++;
+            FriendsPresents frA = presents.Give(n, m);
+            FriendsPresents frB = frA.CloneFriendA();
+            Share(frA, n - 1, 0);
+            Share(frB, n - 1, 1);
+        }
+        public static void Share(FriendsPresents presents, int n, int m)
+        {
+            if (n == 0)
+            {
+                presents.Give(n, m);
+                var.Add(presents);
+                return;
+            }
+            Split(presents, n, m);
+        }
+        public static List<string> Sequences()
+        {
+            List<string> seq = new List<string>();
+            int ind = GetMinIndexes();
+            for (int i = 0; i < var.Count; i++)
+            {
+                if (var[i].getSubstraction() == var[ind].getSubstraction())
+                {
+                    seq.Add(var[i].subSeq(i));                
+                }
+                if((var[i].getSubstraction() == var[ind].getSubstraction()) && var[ind].getSubstraction() > 0)
+                {
+                    seq.Add(var[i].subSeqq(i));
+                }
+            }
+            Console.WriteLine("Mažiausia reikšmė: " + Math.Abs(var[GetMinIndexes()].getSubstraction()));
+            foreach (string item in seq)
+            {
+                Console.WriteLine(item);
+            }
+            return seq;
+        }
+        public static int GetMinIndexes()
+        {
+            int min = var[0].getSubstraction();
+            int ind = 0;
+            for (int i = 0; i < var.Count; i++)
+            {
+                if (Math.Abs(var[i].getSubstraction()) < min)
+                {
+                    min = Math.Abs(var[i].getSubstraction());
+                    ind = i;
+                }
+            }
+            return ind;
+        }
+    }
+}
